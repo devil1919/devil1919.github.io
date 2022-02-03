1c1c3e76796774e5d9eda1d69c75ce4a6b6f054c
devil1919 <chinmaybadwe1234@gmail.com>
2022-02-03 12:55:21 +0000

    Made Modifications

--- ind.js
+++ ind.js
@@ -1,15 +1,4 @@
 
 
-var setval = firebase.database().ref().child("sensors");
-setval.on('child_added',snap => {
-
-   var humidity = snap.child("Crop").val();
-
-   alert(humidity);
-
-
-
-});
-
 
 
